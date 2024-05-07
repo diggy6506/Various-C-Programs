b5cdb17b3d7f95d7d053ebce82f5208c3edf4a5e
Curtis Millership <160729646+diggy6506@users.noreply.github.com>
2024-05-07 20:23:38 +0100

    triangle_checker.c

---
+++ triangle_checker.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+  int main(void) {
+
+  float a, b, c;
+
+
+    printf("\nRemember to only type numbers.\n\n Press Enter to Start\n");
+      scanf("%*c");
+    
+    printf("Enter Length of Side A: ");
+      scanf("%f", &a);
+
+    printf("Enter Length of Side B: ");
+      scanf("%f", &b);
+    
+    printf("Enter Length of Side C: ");
+      scanf("%f", &c);
+
+    
+    
+    
+    if (a == b && a == c) {
+        printf("Your Triangle is Equilateral!");
+    }
+
+    else if (a != b && a != c) {
+        printf("Your Triangle is Scalene!");
+    }
+
+    else
+        printf("Your Triangle is Isoscelses!");
+    
+    
+    
+
+    return 0;
+
+  }
